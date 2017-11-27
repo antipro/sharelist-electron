1595e3ce97dfcca7ed0ec8bf30e8e1d2ad84d7e5
antipro <antipro@163.com>
2017-11-27 15:54:13 +0800

    123

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    transparent: true,
+    frame: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -23,7 +28,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -49,6 +54,7 @@ app.on('window-all-closed', function () {
 })
 
 app.on('activate', function () {
+
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
